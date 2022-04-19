a826fb616dc24f984a93556ed2611fa0f1f0cae1
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-04-19 00:31:16 -0500

    Acance en VentanaTop10

---
+++ Taller4_LightsOut_esqueleto/src/uniandes/dpoo/taller4/interfaz/VentanaTop10.java
@@ -0,0 +1,22 @@
+package uniandes.dpoo.taller4.interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+public class VentanaTop10 extends JFrame{
+	private JPanel panelTop;
+	private JLabel columnaPosicion,columnaNonbre,columnaPuntaje;
+	
+	public VentanaTop10() {
+		setTitle("Top 10");
+		setSize(580,700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        JPanel panelTop
+        setLayout(new GridLayout(11,2));
+        infoCuadricula = new JLabel();
+	}
+}
