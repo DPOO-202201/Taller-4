a5a86ed2cda4827456bfcd8cee69b2c5d3634c50
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-04-18 20:31:43 -0500

    Avance en InfoPartida

--- Taller4_LightsOut_esqueleto/src/uniandes/dpoo/taller4/interfaz/InfoPartida.java
+++ Taller4_LightsOut_esqueleto/src/uniandes/dpoo/taller4/interfaz/InfoPartida.java
@@ -1,5 +1,33 @@
 package uniandes.dpoo.taller4.interfaz;
 
-public class InfoPartida {
+import java.awt.*;
+import javax.swing.*;
 
+
+public class InfoPartida extends JPanel {
+	private JLabel lblJugadas;
+	private int numJugadas;
+	private JTextField txtfNumJugadas;
+	private JLabel lblJugador;
+	private JTextField txtfNomJugador;
+	
+	public InfoPartida() {
+		JPanel panelInfo = new JPanel();
+		panelInfo.setLayout(new GridLayout());
+		lblJugadas = new JLabel ("Jugadas:");
+		numJugadas = 0;
+		txtfNumJugadas = new JTextField (numJugadas);
+		lblJugador = new JLabel ("Jugador:");
+		txtfNomJugador = new JTextField ("");
+		panelInfo.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		panelInfo.add(lblJugadas);
+		panelInfo.add(txtfNumJugadas);
+		panelInfo.add(lblJugador);
+		panelInfo.add(txtfNomJugador);
+	}
+	
+	public void contarJugada() {
+		numJugadas = numJugadas + 1;
+		txtfNumJugadas.setText(String.valueOf(numJugadas));
+	}
 }
