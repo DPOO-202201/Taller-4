a35f6b02d53a17046fd1ac93e628d5aa21c5e857
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-04-19 23:57:19 -0500

    VentanaTop10

--- Taller4_LightsOut_esqueleto/src/uniandes/dpoo/taller4/interfaz/VentanaTop10.java
+++ Taller4_LightsOut_esqueleto/src/uniandes/dpoo/taller4/interfaz/VentanaTop10.java
@@ -1,14 +1,21 @@
 package uniandes.dpoo.taller4.interfaz;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
 
 import javax.swing.*;
 
+import uniandes.dpoo.taller4.modelo.RegistroTop10;
+import uniandes.dpoo.taller4.modelo.Top10;
+
+@SuppressWarnings("serial")
 public class VentanaTop10 extends JFrame{
-	private JPanel panelTop;
-	private JLabel columnaPosicion,columnaNombre,columnaPuntaje;
-	private JLabel infoCuadricula;
+	private JPanel columnaPosiciones,columnaNombres,columnaPuntajes;
+	private JList<String> listaPosiciones,listaNombres,listaPuntajes;
+	private DefaultListModel<String> tempPosiciones,tempNombres,tempPuntajes;
+	private Top10 tempDatos;
 	
 	public VentanaTop10() {
 		setTitle("Top 10");
@@ -16,14 +23,44 @@ public class VentanaTop10 extends JFrame{
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         
-        JPanel panelTop;
-        setLayout(new GridLayout(11,3));
-        columnaPosicion = new JLabel("Posicion");
-        columnaNombre = new JLabel("Nombre");
-        columnaPuntaje = new JLabel("Puntaje");
+        columnaPosiciones = new JPanel();
+        columnaNombres = new JPanel();
+        columnaPuntajes = new JPanel();
         
-        add(columnaPosicion);
-        add(columnaNombre);
-        add(columnaPuntaje);
+        tempPosiciones = new DefaultListModel<String>();
+        tempNombres = new DefaultListModel<String>();
+        tempPuntajes = new DefaultListModel<String>();
+        
+		listaPosiciones = new JList<String>(tempPosiciones);
+		listaNombres = new JList<String>(tempNombres);
+		listaPuntajes = new JList<String>(tempPuntajes);
+		
+		tempPosiciones.addElement("Posicion");
+		tempNombres.addElement("Nombre");
+		tempPuntajes.addElement("Puntaje");
+        
+		tempDatos = new Top10();
+		File archivo = new File("data\top10.csv");
+		tempDatos.cargarRecords(archivo);
+        
+        Collection<RegistroTop10> tempRegistro = tempDatos.darRegistros();
+        
+        for (int posicionActual = 1; posicionActual <=10 ; posicionActual = posicionActual + 1) {
+        	Iterator<RegistroTop10> j = tempRegistro.iterator();
+        	if (j.hasNext()) {
+        	tempNombres.addElement((j.next().darNombre()));
+        	tempPuntajes.addElement(String.valueOf((j.next().darPuntos())));
+       		tempPosiciones.addElement(String.valueOf(posicionActual));
+       		j.next();
+        	}
+        }
+        
+        columnaPosiciones.add(listaPosiciones);
+        columnaNombres.add(listaNombres);
+        columnaPuntajes.add(listaPuntajes);
+        
+        add(columnaPosiciones,BorderLayout.WEST);
+        add(columnaNombres,BorderLayout.CENTER);
+        add(columnaPuntajes,BorderLayout.EAST);
 	}
 }
